80d05b05f85948bc1cc5f1c32339fdc07a4054fe
Wildan Ade Mamora <ademamorawildan@gmail.com>
2026-07-12 22:00:50 +0700

    commit

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -1,38 +1,40 @@
+import kerupuk from '../assets/gallery/kerupuk-ikan.jpeg'
+
 export const gallery = [
   {
     id: 1,
     title: "Produksi Kerupuk Ikan",
     category: "Produksi",
-    image: "/src/assets/gallery/kerupuk-ikan.jpeg"
+    image: kerupuk
   },
   {
     id: 2,
     title: "Pembuatan Petis",
     category: "Produksi",
-    image: "/src/assets/gallery/kerupuk-ikan.jpeg"
+    image: kerupuk
   },
   {
     id: 3,
     title: "Pameran UMKM",
     category: "Kegiatan",
-    image: "/src/assets/gallery/kerupuk-ikan.jpeg"
+    image: kerupuk
   },
   {
     id: 4,
     title: "Pelatihan UMKM",
     category: "Kegiatan",
-    image: "/src/assets/gallery/kerupuk-ikan.jpeg"
+    image: kerupuk
   },
   {
     id: 5,
     title: "Produk Songkok",
     category: "Produk",
-    image: "/src/assets/gallery/kerupuk-ikan.jpeg"
+    image: kerupuk
   },
   {
     id: 6,
     title: "Produk Kerupuk",
     category: "Produk",
-    image: "/src/assets/gallery/kerupuk-ikan.jpeg"
+    image: kerupuk
   }
 ]
