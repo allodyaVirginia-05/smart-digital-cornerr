334d55008179a92def60f962d4e851bbc98efe34
Wildan Ade Mamora <ademamorawildan@gmail.com>
2026-07-15 07:50:09 +0700

    commit

--- src/data/products.js
+++ src/data/products.js
@@ -1,11 +1,63 @@
-import kerupukIkan from '../assets/products/kerupuk-ikan-laut-srowo.jpeg'
+import product1 from '../assets/products/produk-1.jpeg'
+import product2 from '../assets/products/produk-2.jpeg'
+import product3 from '../assets/products/produk-3.jpeg'
+import product4 from '../assets/products/produk-4.jpeg'
+import product5 from '../assets/products/produk-5.jpeg'
+import product6 from '../assets/products/produk-6.jpeg'
+import product7 from '../assets/products/produk-7.jpeg'
+import product8 from '../assets/products/produk-8.jpeg'
+import product9 from '../assets/products/produk-9.jpeg'
+import product10 from '../assets/products/produk-10.jpeg'
+import product11 from '../assets/products/produk-11.jpeg'
+import product12 from '../assets/products/produk-12.jpeg'
+import product13 from '../assets/products/produk-13.jpeg'
+import product14 from '../assets/products/produk-14.jpeg'
+import product15 from '../assets/products/produk-15.jpeg'
+import product16 from '../assets/products/produk-16.jpeg'
+import product17 from '../assets/products/produk-17.jpeg'
+import product18 from '../assets/products/produk-18.jpeg'
+import product19 from '../assets/products/produk-19.jpeg'
+import product20 from '../assets/products/produk-20.jpeg'
+import product21 from '../assets/products/produk-21.jpeg'
+import product22 from '../assets/products/produk-22.jpeg'
+import product23 from '../assets/products/produk-23.jpeg'
+// import product24 from '../assets/products/produk-24.jpeg'
+// import product25 from '../assets/products/produk-25.jpeg'
+// import product26 from '../assets/products/produk-26.jpeg'
+// import product27 from '../assets/products/produk-27.jpeg'
+// import product28 from '../assets/products/produk-28.jpeg'
+// import product29 from '../assets/products/produk-29.jpeg'
+// import product30 from '../assets/products/produk-30.jpeg'
+// import product31 from '../assets/products/produk-31.jpeg'
+// import product32 from '../assets/products/produk-32.jpeg'
+// import product33 from '../assets/products/produk-33.jpeg'
+// import product34 from '../assets/products/produk-34.jpeg'
+// import product35 from '../assets/products/produk-35.jpeg'
+// import product36 from '../assets/products/produk-36.jpeg'
+// import product37 from '../assets/products/produk-37.jpeg'
+// import product38 from '../assets/products/produk-38.jpeg'
+// import product39 from '../assets/products/produk-39.jpeg'
+// import product40 from '../assets/products/produk-40.jpeg'
+// import product41 from '../assets/products/produk-41.jpeg'
+// import product42 from '../assets/products/produk-42.jpeg'
+// import product43 from '../assets/products/produk-43.jpeg'
+// import product44 from '../assets/products/produk-44.jpeg'
+// import product45 from '../assets/products/produk-45.jpeg'
+// import product46 from '../assets/products/produk-46.jpeg'
+// import product47 from '../assets/products/produk-47.jpeg'
+// import product48 from '../assets/products/produk-48.jpeg'
+// import product49 from '../assets/products/produk-49.jpeg'
+// import product50 from '../assets/products/produk-50.jpeg'
+// import product51 from '../assets/products/produk-51.jpeg'
+// import product52 from '../assets/products/produk-52.jpeg'
+// import product53 from '../assets/products/produk-53.jpeg'
 
 export const products = [
   {
     id: 1,
     name: 'Kerupuk Ikan',
     category: 'Makanan',
-    image: kerupukIkan,
+    image: product1,
 
     description:
       'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
@@ -18,21 +70,14 @@ export const products = [
         name: 'UMKM Mina Jaya',
         owner: 'Bapak Ahmad',
       },
-      {
-        name: 'UMKM Sumber Laut',
-        owner: 'Ibu Siti',
-      },
-      {
-        name: 'UMKM Berkah Bahari',
-        owner: 'Bapak Hasan',
-      },
     ],
   },
   {
     id: 2,
     name: 'Kerupuk Ikan',
     category: 'Makanan',
-    image: kerupukIkan,
+    image: product2
+    ,
 
     description:
       'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
@@ -59,7 +104,7 @@ export const products = [
     id: 3,
     name: 'Kerupuk Ikan',
     category: 'Makanan',
-    image: kerupukIkan,
+    image: product3,
 
     description:
       'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
@@ -69,16 +114,8 @@ export const products = [
 
     umkm: [
       {
-        name: 'UMKM Mina Jaya',
-        owner: 'Bapak Ahmad',
-      },
-      {
-        name: 'UMKM Sumber Laut',
-        owner: 'Ibu Siti',
-      },
-      {
-        name: 'UMKM Berkah Bahari',
-        owner: 'Bapak Hasan',
+        name: 'UD. MASKANAH GROUP',
+        owner: 'Maskanah',
       },
     ],
   },
@@ -86,7 +123,7 @@ export const products = [
     id: 4,
     name: 'Kerupuk Ikan',
     category: 'Makanan',
-    image: kerupukIkan,
+    image: product4,
 
     description:
       'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
@@ -113,7 +150,461 @@ export const products = [
     id: 5,
     name: 'Kerupuk Ikan',
     category: 'Makanan',
-    image: kerupukIkan,
+    image: product5,
+
+    description:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    detailDescription:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    umkm: [
+      {
+        name: 'UD. MUSA HERMANTO',
+        owner: 'INNAMA ARFAAH',
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: 'Kerupuk Ikan',
+    category: 'Makanan',
+    image: product6,
+
+    description:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    detailDescription:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    umkm: [
+      {
+        name: 'UD. MBAK NUR',
+        owner: 'NUR AFIYAH',
+      },
+    ],
+  },
+  {
+    id: 7,
+    name: 'Kerupuk Ikan',
+    category: 'Makanan',
+    image: product7,
+
+    description:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    detailDescription:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    umkm: [
+      {
+        name: 'UMKM Mina Jaya',
+        owner: 'Bapak Ahmad',
+      },
+      {
+        name: 'UMKM Sumber Laut',
+        owner: 'Ibu Siti',
+      },
+      {
+        name: 'UMKM Berkah Bahari',
+        owner: 'Bapak Hasan',
+      },
+    ],
+  },
+  {
+    id: 8,
+    name: 'Kerupuk Ikan',
+    category: 'Makanan',
+    image: product8 ,
+
+    description:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    detailDescription:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    umkm: [
+      {
+        name: 'UD. DINDA BAROKAH',
+        owner: 'AKHMAD HANAFI',
+      },
+    ],
+  },
+  {
+    id: 9,
+    name: 'Kerupuk Ikan',
+    category: 'Makanan',
+    image: product9,
+
+    description:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    detailDescription:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    umkm: [
+      {
+        name: 'UMKM Mina Jaya',
+        owner: 'Bapak Ahmad',
+      },
+      {
+        name: 'UMKM Sumber Laut',
+        owner: 'Ibu Siti',
+      },
+      {
+        name: 'UMKM Berkah Bahari',
+        owner: 'Bapak Hasan',
+      },
+    ],
+  },
+  {
+    id: 10,
+    name: 'Kerupuk Ikan',
+    category: 'Makanan',
+    image: product10,
+
+    description:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    detailDescription:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    umkm: [
+      {
+        name: 'UMKM Mina Jaya',
+        owner: 'Bapak Ahmad',
+      },
+      {
+        name: 'UMKM Sumber Laut',
+        owner: 'Ibu Siti',
+      },
+      {
+        name: 'UMKM Berkah Bahari',
+        owner: 'Bapak Hasan',
+      },
+    ],
+  },
+  {
+    id: 11,
+    name: 'Kerupuk Ikan',
+    category: 'Makanan',
+    image: product11,
+
+    description:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    detailDescription:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    umkm: [
+      {
+        name: 'UD. MAJU JAYA',
+        owner: 'SUKANAH',
+      },
+    ],
+  },
+  {
+    id: 12,
+    name: 'Kerupuk Ikan',
+    category: 'Makanan',
+    image: product12,
+
+    description:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    detailDescription:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    umkm: [
+      {
+        name: 'UMKM Mina Jaya',
+        owner: 'Bapak Ahmad',
+      },
+      {
+        name: 'UMKM Sumber Laut',
+        owner: 'Ibu Siti',
+      },
+      {
+        name: 'UMKM Berkah Bahari',
+        owner: 'Bapak Hasan',
+      },
+    ],
+  },
+  {
+    id: 13,
+    name: 'Kerupuk Ikan',
+    category: 'Makanan',
+    image: product13,
+
+    description:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    detailDescription:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    umkm: [
+      {
+        name: 'UMKM Mina Jaya',
+        owner: 'Bapak Ahmad',
+      },
+      {
+        name: 'UMKM Sumber Laut',
+        owner: 'Ibu Siti',
+      },
+      {
+        name: 'UMKM Berkah Bahari',
+        owner: 'Bapak Hasan',
+      },
+    ],
+  },
+  {
+    id: 14,
+    name: 'Kerupuk Ikan',
+    category: 'Makanan',
+    image: product14,
+
+    description:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    detailDescription:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    umkm: [
+      {
+        name: 'UMKM Mina Jaya',
+        owner: 'Bapak Ahmad',
+      },
+      {
+        name: 'UMKM Sumber Laut',
+        owner: 'Ibu Siti',
+      },
+      {
+        name: 'UMKM Berkah Bahari',
+        owner: 'Bapak Hasan',
+      },
+    ],
+  },
+  {
+    id: 15,
+    name: 'Kerupuk Ikan',
+    category: 'Makanan',
+    image: product15,
+
+    description:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    detailDescription:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    umkm: [
+      {
+        name: 'UMKM Mina Jaya',
+        owner: 'Bapak Ahmad',
+      },
+      {
+        name: 'UMKM Sumber Laut',
+        owner: 'Ibu Siti',
+      },
+      {
+        name: 'UMKM Berkah Bahari',
+        owner: 'Bapak Hasan',
+      },
+    ],
+  },
+  {
+    id: 16,
+    name: 'Kerupuk Ikan',
+    category: 'Makanan',
+    image: product16,
+
+    description:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    detailDescription:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    umkm: [
+      {
+        name: 'UMKM Mina Jaya',
+        owner: 'Bapak Ahmad',
+      },
+      {
+        name: 'UMKM Sumber Laut',
+        owner: 'Ibu Siti',
+      },
+      {
+        name: 'UMKM Berkah Bahari',
+        owner: 'Bapak Hasan',
+      },
+    ],
+  },
+  {
+    id: 17,
+    name: 'Kerupuk Ikan',
+    category: 'Makanan',
+    image: product17,
+
+    description:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    detailDescription:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    umkm: [
+      {
+        name: 'UMKM Mina Jaya',
+        owner: 'Bapak Ahmad',
+      },
+      {
+        name: 'UMKM Sumber Laut',
+        owner: 'Ibu Siti',
+      },
+      {
+        name: 'UMKM Berkah Bahari',
+        owner: 'Bapak Hasan',
+      },
+    ],
+  },
+  {
+    id: 18,
+    name: 'Kerupuk Ikan',
+    category: 'Makanan',
+    image: product18,
+
+    description:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    detailDescription:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    umkm: [
+      {
+        name: 'UMKM Mina Jaya',
+        owner: 'Bapak Ahmad',
+      },
+      {
+        name: 'UMKM Sumber Laut',
+        owner: 'Ibu Siti',
+      },
+      {
+        name: 'UMKM Berkah Bahari',
+        owner: 'Bapak Hasan',
+      },
+    ],
+  },
+  {
+    id: 19,
+    name: 'Kerupuk Ikan',
+    category: 'Makanan',
+    image: product19,
+
+    description:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    detailDescription:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    umkm: [
+      {
+        name: 'UMKM Mina Jaya',
+        owner: 'Bapak Ahmad',
+      },
+      {
+        name: 'UMKM Sumber Laut',
+        owner: 'Ibu Siti',
+      },
+      {
+        name: 'UMKM Berkah Bahari',
+        owner: 'Bapak Hasan',
+      },
+    ],
+  },
+  {
+    id: 20,
+    name: 'Kerupuk Ikan',
+    category: 'Makanan',
+    image: product20,
+
+    description:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    detailDescription:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    umkm: [
+      {
+        name: 'UMKM Mina Jaya',
+        owner: 'Bapak Ahmad',
+      },
+      {
+        name: 'UMKM Sumber Laut',
+        owner: 'Ibu Siti',
+      },
+      {
+        name: 'UMKM Berkah Bahari',
+        owner: 'Bapak Hasan',
+      },
+    ],
+  },
+  {
+    id: 21,
+    name: 'Kerupuk Ikan',
+    category: 'Makanan',
+    image: product21,
+
+    description:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    detailDescription:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    umkm: [
+      {
+        name: 'UMKM Mina Jaya',
+        owner: 'Bapak Ahmad',
+      },
+      {
+        name: 'UMKM Sumber Laut',
+        owner: 'Ibu Siti',
+      },
+      {
+        name: 'UMKM Berkah Bahari',
+        owner: 'Bapak Hasan',
+      },
+    ],
+  },
+  {
+    id: 22,
+    name: 'Kerupuk Ikan',
+    category: 'Makanan',
+    image: product22,
+
+    description:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    detailDescription:
+      'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
+
+    umkm: [
+      {
+        name: 'UMKM Mina Jaya',
+        owner: 'Bapak Ahmad',
+      },
+      {
+        name: 'UMKM Sumber Laut',
+        owner: 'Ibu Siti',
+      },
+      {
+        name: 'UMKM Berkah Bahari',
+        owner: 'Bapak Hasan',
+      },
+    ],
+  },
+  {
+    id: 23,
+    name: 'Kerupuk Ikan',
+    category: 'Makanan',
+    image: product23,
 
     description:
       'kerupuk ikan laut khas sidayu terbuat dari ikan laut segar pilihan kaya protein. rasa yang gurih dan renyah cocok sebagai cemilan maupun teman saat makan',
